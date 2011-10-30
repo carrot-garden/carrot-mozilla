1b1861ab6009e0d03b71b3b5fbf98177b6edd54e
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2011-10-30 16:59:24 -0500

    a

--- carrot-mozilla-mail-filter/src/main/omni/content/modules/mail.js
+++ carrot-mozilla-mail-filter/src/main/omni/content/modules/mail.js
@@ -25,10 +25,18 @@ function test(window) {
 
 };
 
-function copyPopupNameAndEmail(window, portion) {
+function findEmailNode(window) {
 
 	var node = window.findEmailNodeFromPopupNode(window.document.popupNode,
 			'emailAddressPopup');
+
+	return node;
+
+}
+
+function copyPopupNameAndEmail(window, portion) {
+
+	var node = findEmailNode(window);
 
 	var emailAddress = node.getAttribute("emailAddress");
 	var displayName = node.getAttribute("displayName");
@@ -55,8 +63,7 @@ function copyPopupNameAndEmail(window, portion) {
 
 function openPopupWebSiteFromEmail(window) {
 
-	var node = window.findEmailNodeFromPopupNode(window.document.popupNode,
-			'emailAddressPopup');
+	var node = findEmailNode(window);
 
 	var emailAddress = node.getAttribute("emailAddress");
 
@@ -66,36 +73,46 @@ function openPopupWebSiteFromEmail(window) {
 
 }
 
-function makePopupMenuitems(window) {
+const
+TYPE = {
+	news : 'Add News',
+	vendor : 'Add Vendor',
+	employer : 'Add Employer',
+};
+
+function makePopupMenuitem(window, type) {
 
 	var document = window.document;
-
-	// var node = window.findEmailNodeFromPopupNode(document.popupNode,
-	// 'emailAddressPopup');
-
-	// var emailAddress = node.getAttribute("emailAddress");
-	// var displayName = node.getAttribute("displayName");
-
 	var XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
-
 	var popupId = "${package}.emailAddressPopup.menupopup";
-
 	var menupopup = document.getElementById(popupId);
 
 	var menuitem = document.createElementNS(XUL_NS, "menuitem");
 
-	var type = "XXX";
 	menuitem.setAttribute("id", popupId + "." + type);
-	menuitem.setAttribute("label", type);
+	menuitem.setAttribute("label", TYPE[type]);
 	menuitem.setAttribute("oncommand",
 			"window['${package}'].mail.workPopupMenuitems(window,'" + type
 					+ "');");
 
 	menupopup.appendChild(menuitem);
+	
+	return menuitem;
+
+}
+
+function makePopupMenuitems(window) {
+
+	for (type in TYPE) {
+		makePopupMenuitem(window, type);
+	}
 
 }
 
 function workPopupMenuitems(window, type) {
+
+	const
+	document = window.document;
 
 	window.alert("hello : " + type);
 
@@ -104,5 +121,11 @@ function workPopupMenuitems(window, type) {
 function init(window) {
 
 	makePopupMenuitems(window);
+
+}
+
+function makeEmployer(window) {
+
+	window.alert("hello employer ");
 
 }
