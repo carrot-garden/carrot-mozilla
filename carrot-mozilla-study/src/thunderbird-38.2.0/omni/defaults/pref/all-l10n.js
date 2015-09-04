b58817f49c2c1f988c933c078c8196de949dabcb
Andrei Pozolotin <Andrei.Pozolotin@gmail.com>
2015-09-04 01:11:18 +0000

    a

---
+++ carrot-mozilla-study/src/thunderbird-38.2.0/omni/defaults/pref/all-l10n.js
@@ -0,0 +1,6 @@
+//@line 4 "/builds/slave/tb-rel-c-esr38-lx_bld-00000000/build/mail/locales/en-US/all-l10n.js"
+
+//@line 6 "/builds/slave/tb-rel-c-esr38-lx_bld-00000000/build/mail/locales/en-US/all-l10n.js"
+
+pref("general.useragent.locale", "en-US");
+pref("spellchecker.dictionary", "en-US");
---
+++ carrot-mozilla-study/src/thunderbird-38.2.0/omni/modules/SearchIntegration.js
@@ -0,0 +1,13 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var EXPORTED_SYMBOLS = ["SearchIntegration"];
+
+//@line 11 "/builds/slave/tb-rel-c-esr38-lx_bld-00000000/build/mail/components/search/SearchIntegration.js"
+
+//@line 16 "/builds/slave/tb-rel-c-esr38-lx_bld-00000000/build/mail/components/search/SearchIntegration.js"
+// Set SearchIntegration to null, as we don't have it
+var SearchIntegration = null;
+//@line 19 "/builds/slave/tb-rel-c-esr38-lx_bld-00000000/build/mail/components/search/SearchIntegration.js"
+
